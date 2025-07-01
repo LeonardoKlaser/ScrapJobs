90b58284916d1f4869cb0ee019e570433f40fef3
LeonardoKlaser <leobkklaser@gmail.com>
2025-07-01 13:35:37 -0300

    feat: first configuration ci/cd deploy ec2 with gb actions

--- infra/terraform/user_data.sh
+++ infra/terraform/user_data.sh
@@ -1,9 +1,9 @@
 #!/bin/dash
 
 sudo su
-yum update -y
-yum install -y docker
-service docker start
+sudo yum update -y
+sudo yum install -y docker jq
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo service docker start
 usermod -a -G docker ec2-user
-
-docker run -p 80:8080 {url imagem docker}
