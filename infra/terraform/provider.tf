61c5701de0ec019f1e4782a9484037d772ad9b55
LeoKlaser <leobkklaser@gmail.com>
2025-07-07 22:41:08 -0300

    comment backend s3 to include at second push

--- infra/terraform/provider.tf
+++ infra/terraform/provider.tf
@@ -8,13 +8,13 @@ terraform {
 
   # Esta é a parte mais importante.
   # Ela instrui o Terraform a usar o S3 e o DynamoDB.
-  backend "s3" {
-    bucket         = "${var.project_name}-terraform-state-bucket" # Coloque o mesmo nome do bucket definido acima
-    key            = "global/s3/terraform.tfstate"         # O caminho/nome do arquivo de estado dentro do bucket
-    region         = "us-east-1"
-    dynamodb_table = "${var.project_name}-terraform-lock"     # O nome da tabela do DynamoDB definida acima
-    encrypt        = true
-  }
+#   backend "s3" {
+#     bucket         = "${var.project_name}-terraform-state-bucket" # Coloque o mesmo nome do bucket definido acima
+#     key            = "global/s3/terraform.tfstate"         # O caminho/nome do arquivo de estado dentro do bucket
+#     region         = "us-east-1"
+#     dynamodb_table = "${var.project_name}-terraform-lock"     # O nome da tabela do DynamoDB definida acima
+#     encrypt        = true
+#   }
 }
 
 provider "aws" {
