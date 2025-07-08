67bb288ad44a954977c46e99c90efd25917e9cc8
LeoKlaser <leobkklaser@gmail.com>
2025-07-07 23:27:40 -0300

    data current

--- infra/terraform/iam.tf
+++ infra/terraform/iam.tf
@@ -95,3 +95,5 @@ resource "aws_iam_instance_profile" "ec2_profile" {
     name = "${var.project_name}-ec2-profile"
     role = aws_iam_role.ec2_role.name
 }
+
+data "aws_caller_identity" "current" {}
