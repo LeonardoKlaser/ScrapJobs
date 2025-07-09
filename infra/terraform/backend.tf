36a050a99cdac02731f6884e2bf5d2487a22bd12
LeoKlaser <leobkklaser@gmail.com>
2025-07-08 22:03:18 -0300

    remove s3 for now

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -1,25 +1,25 @@
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "scrapjobs-terraform-state-bucket"
+# resource "aws_s3_bucket" "terraform_state" {
+#   bucket = "scrapjobs-terraform-state-bucket"
 
-  lifecycle {
-    prevent_destroy = true
-  }
-}
+#   lifecycle {
+#     prevent_destroy = true
+#   }
+# }
 
-resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
-    bucket = aws_s3_bucket.terraform_state.id
-    versioning_configuration {
-        status = "Enabled"
-    }
-}
+# resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
+#     bucket = aws_s3_bucket.terraform_state.id
+#     versioning_configuration {
+#         status = "Enabled"
+#     }
+# }
 
-resource "aws_dynamodb_table" "terraform_state_lock" {
-  name           = "scrapjobs-terraform-lock"
-  billing_mode   = "PAY_PER_REQUEST" 
-  hash_key       = "LockID"
+# resource "aws_dynamodb_table" "terraform_state_lock" {
+#   name           = "scrapjobs-terraform-lock"
+#   billing_mode   = "PAY_PER_REQUEST" 
+#   hash_key       = "LockID"
 
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
+#   attribute {
+#     name = "LockID"
+#     type = "S"
+#   }
+# }
