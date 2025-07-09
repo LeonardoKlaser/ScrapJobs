a792675a21dad1a858f1647d402a53026b101f9f
LeoKlaser <leobkklaser@gmail.com>
2025-07-08 21:56:56 -0300

    fix s3 name v4

--- infra/terraform/provider.tf
+++ infra/terraform/provider.tf
@@ -7,13 +7,13 @@ terraform {
   }
 
   #instrui o Terraform a usar o S3 e o DynamoDB.
-  backend "s3" {
-    bucket         = "scrapjobs-terraform-state-bucket" # Coloque o mesmo nome do bucket definido acima
-    key            = "global/s3/terraform.tfstate"         # O caminho/nome do arquivo de estado dentro do bucket
-    region         = "us-east-1"
-    dynamodb_table = "scrapjobs-terraform-lock"     # O nome da tabela do DynamoDB definida acima
-    encrypt        = true
-  }
+  # backend "s3" {
+  #   bucket         = "scrapjobs-terraform-state-bucket" # Coloque o mesmo nome do bucket definido acima
+  #   key            = "global/s3/terraform.tfstate"         # O caminho/nome do arquivo de estado dentro do bucket
+  #   region         = "us-east-1"
+  #   dynamodb_table = "scrapjobs-terraform-lock"     # O nome da tabela do DynamoDB definida acima
+  #   encrypt        = true
+  # }
 }
 
 provider "aws" {
